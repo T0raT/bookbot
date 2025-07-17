9ad295194714116123b3457bcc86b6303f9ce601
T0raT <tigerliu@bu.edu>
2025-07-17 16:21:53 -0400

    feat: added function to count all occurences of each character in a string/text

--- stats.py
+++ stats.py
@@ -3,3 +3,14 @@ def book_word_count(book_text: str):
     # and returns the number of words in it.
     split_text = book_text.split()
     return len(split_text)
+
+def book_char_count(book_text: str):
+    # Takes a string containing the text of a book 
+    # and counts the occurances of each character
+    char_count = {}
+    for c in book_text:
+        if c not in char_count:
+            char_count[c] = 1
+        else:
+            char_count[c] += 1
+    return char_count
