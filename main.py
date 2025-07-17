5edbdc90682002fbd1bf418939735ceec5f9dd42
T0raT <tigerliu@bu.edu>
2025-07-17 16:23:56 -0400

    refactor: now imports stats.py as a module not just function to avoid namespace issues

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import book_word_count
+import stats # never really cared but import this way to not have functions fully take over the namespace
+# This is a little more verbose than "from stats import *"
+# Wether this is industry standard or not, idk.
 
 def get_book_text(book_path: str):
     # Takes a path to a book file and returns its content as a string.
@@ -8,8 +10,10 @@ def get_book_text(book_path: str):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    word_count = book_word_count(book_text)
+    word_count = stats.book_word_count(book_text)
+    char_count = stats.book_char_count(book_text)
     print(f"{word_count} words found in the document")
+    print(char_count)
     return None
 
 if __name__ == "__main__":
