3c473372130fc5355790ad35f3ad19337a1be24d
T0raT <tigerliu@bu.edu>
2025-07-18 16:09:04 -0400

    feat: now returns an array of sorted dictionary for each character

--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+def sort_on(dictionary):
+    return dictionary["num"]
+
 def book_word_count(book_text: str):
     # Takes a string containing the text of a book 
     # and returns the number of words in it.
@@ -15,3 +18,14 @@ def book_char_count(book_text: str):
         else:
             char_count[c] += 1
     return char_count
+
+def sorted_char_count(char_count_dict: dict):
+    # Sorts the character count dictionary by the number of occurrences
+    # Descending order.
+    
+    sorted_char_count = []
+    for c in char_count_dict:
+        sorted_char_count.append({"name":c, "num": char_count_dict[c]})
+
+    sorted_char_count.sort(key=sort_on, reverse=True)
+    return sorted_char_count
